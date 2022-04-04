d966f7214cac1e71d839851b769d357a8442cce0
lateminer <9951982+lateminer@users.noreply.github.com>
2022-04-04 12:56:25 +0300

    Set HF3_1 and 3_1_1 time on testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -209,8 +209,8 @@ public:
         consensus.nProtocolV1RetargetingFixedTime = 1395631999;
         consensus.nProtocolV2Time = 1407053625;
         consensus.nProtocolV3Time = 1444028400;
-        consensus.nProtocolV3_1Time = 4102437600;
-        consensus.nProtocolV3_1_1Time = 4102437600;
+        consensus.nProtocolV3_1Time = 1649030400;
+        consensus.nProtocolV3_1_1Time = 1649030400;
         consensus.nProtocolV3_1_2Time = 4102437600;
         consensus.nLastPOWBlock = 0x7fffffff;
         consensus.nStakeTimestampMask = 0xf;
